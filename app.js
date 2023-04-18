f6b7d9c17ab1b502976aa0a8b9490b00f4a6a7ce
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-04-18 16:49:32 +0530

    Final Commit of the project

---
+++ app.js
@@ -0,0 +1,91 @@
+let input = document.getElementById('input');
+
+input.addEventListener('change',()=>{
+    search();
+})
+
+function search() {
+    let input = document.getElementById('input').value
+    input=input.toLowerCase();
+    console.log(input)
+    let x = document.getElementsByClassName('hover-header');
+    console.log(x)
+    let y = document.getElementsByClassName('portfolio-item');
+    console.log(y)
+    for (i = 0; i < x.length; i++) { 
+        console.log(x[i].innerHTML)
+        console.log(y[i])
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            y[i].style.display="none";
+        }
+        else {
+            y[i].style.display="grid";                 
+        }
+    }
+}
+
+
+let allbtn = document.getElementsByClassName("allbtn");
+let htmlbtn = document.getElementsByClassName('htmlbtn');
+let javabtn = document.getElementsByClassName("javabtn");
+let javatddbtn = document.getElementsByClassName('javatddbtn')
+
+javatddbtn[0].addEventListener('click',()=>{
+    let htmlclass = document.getElementsByClassName("html")
+    let javaclass = document.getElementsByClassName("java");
+    let javatddclass = document.getElementsByClassName("javatdd")
+    for(let i=0;i<javaclass.length;i++){
+        javaclass[i].style.display="none";
+    }
+    for(let i=0;i<htmlclass.length;i++){
+        htmlclass[i].style.display = "none";
+    }
+    for(let i=0;i<javatddclass.length;i++){
+        javatddclass[i].style.display = "grid";
+    }
+})
+
+htmlbtn[0].addEventListener('click',()=>{
+    let htmlclass = document.getElementsByClassName("html")
+    let javaclass = document.getElementsByClassName("java");
+    let javatddclass = document.getElementsByClassName("javatdd");
+    for(let i=0;i<htmlclass.length;i++){
+        htmlclass[i].style.display="grid";
+    }
+    for(let i=0;i<javaclass.length;i++){
+        javaclass[i].style.display = "none";
+    }
+    for(let i=0;i<javatddclass.length;i++){
+        javatddclass[i].style.display = "none";
+    }
+})
+
+javabtn[0].addEventListener('click',()=>{
+    let htmlclass = document.getElementsByClassName("html")
+    let javaclass = document.getElementsByClassName("java");
+    let javatddclass = document.getElementsByClassName("javatdd");
+    for(let i=0;i<javaclass.length;i++){
+        javaclass[i].style.display="grid";
+    }
+    for(let i=0;i<htmlclass.length;i++){
+        htmlclass[i].style.display = "none";
+    }
+    for(let i=0;i<javatddclass.length;i++){
+        javatddclass[i].style.display = "none";
+    }
+})
+
+allbtn[0].addEventListener('click',()=>{
+    let htmlclass = document.getElementsByClassName("html")
+    let javaclass = document.getElementsByClassName("java");
+    let javatddclass = document.getElementsByClassName("javatdd");
+    for(let i=0;i<javaclass.length;i++){
+        javaclass[i].style.display="grid";
+    }
+    for(let i=0;i<htmlclass.length;i++){
+        htmlclass[i].style.display = "grid";
+    }
+    for(let i=0;i<javatddclass.length;i++){
+        javatddclass[i].style.display = "grid";
+    }
+})
